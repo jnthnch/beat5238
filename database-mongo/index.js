4f3c9b769a3deeae5d6b4cb54f9dc2ef75124f28
jnthnch <jchu127@gmail.com>
2019-05-06 19:53:57 -0700

    Write query functions for express endpoints

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -3,24 +3,24 @@ mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
 
-db.on('error', function() {
+db.on('error', function () {
   console.log('mongoose connection error');
 });
 
-db.once('open', function() {
+db.once('open', function () {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
+var userSchema = mongoose.Schema({
+  name: Number,
   description: String
 });
 
-var Item = mongoose.model('Item', itemSchema);
+var User = mongoose.model('User', userSchema);
 
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
+var selectAll = function (callback) {
+  User.find({}, function (err, items) {
+    if (err) {
       callback(err, null);
     } else {
       callback(null, items);
--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,19 +1,38 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'FILL_ME_IN',
-  database : 'test'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'sportspicks'
 });
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
+module.exports = {
+  selectAll: function (callback) {
+    connection.query('SELECT * FROM users', function (err, results, fields) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, results);
+      }
+    });
+  },
+  postUser: function (params, callback) {
+    let userParams = Object.values(params);
+    let queryStr = 'insert into users(first_name, last_name, username) values (?, ?, ?)';
+    connection.query(queryStr, userParams, function (err, results) {
+      callback(err, results);
+    });
+  },
+  postRecord: function (username, callback) {
+    let user_id = connection.query(`SELECT id FROM users WHERE username = ${username}`)
+    let addRecordQueryStr = 'insert into records(wins, losses, ties, user_id)  (?, ?, ?, ?)';
+    connection.query(addRecordQueryStr, 0, 0, 0, user_id, function (err, results) {
+      callback(err, results);
+    })
+  }
 };
 
+
+
+// module.exports.selectAll = selectAll;
