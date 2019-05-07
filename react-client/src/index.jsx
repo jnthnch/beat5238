d8fbb97c6c0cb25f5961dadb404f3aa6db9f3383
jnthnch <jchu127@gmail.com>
2019-05-06 19:54:41 -0700

    Import 3 components to index.jsx

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,34 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import NameForm from './components/NameForm.jsx';
+import UsernameDropdown from './components/UsernameDropdown.jsx';
+import GameList from './components/GameList.jsx';
+import GameSlate from './sample_data';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      users: [],
+      games: []
     }
+    this.handleUsernameSelection = this.handleUsernameSelection.bind(this);
   }
 
   componentDidMount() {
     $.ajax({
-      url: '/items',
+      url: '/users',
       success: (data) => {
         this.setState({
-          items: data
+          users: data,
+          games: GameSlate
         })
       },
       error: (err) => {
         console.log('err', err);
       }
     });
+
+    // $.ajax({
+    //   url: '/games',
+    //   success: (data) => {
+    //     let games = data.data.filter(game => game.sport_key === "basketball_nba");
+    //     this.setState({
+    //       games: games
+    //     })
+    //   },
+    //   error: (err) => {
+    //     console.log('err', err);
+    //   }
+    // });
+  }
+
+  handleUsernameSelection() {
+
   }
 
   render() {
-    return (<div>
-      <h1>Weekly Sports Picks</h1>
-      <NameForm />
-    </div>)
+    return (
+      <div>
+        <h1>52.38% Weekly Sports Picks</h1>
+        <NameForm />
+        <UsernameDropdown users={this.state.users} handleUsernameSelection={this.handleUsernameSelection} />
+        <GameList games={this.state.games} />
+      </div>
+    )
   }
 }
 
