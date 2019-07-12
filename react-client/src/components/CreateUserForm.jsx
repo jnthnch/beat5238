40de3ce430778adabab038704bd9eaf4404f993c
jnthnch <jchu127@gmail.com>
2019-07-11 19:39:09 -0700

    Add handleInputChange function to forms

--- react-client/src/components/CreateUserForm.jsx
+++ react-client/src/components/CreateUserForm.jsx
@@ -1,47 +1,94 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import $ from 'jquery';
 
-function CreateUserForm(props) {
-  return (
-    <div className="wrapper">
-      <h1 className="header">
-        Beat5238
-      </h1>
-      <div>
-        <form className="forms">
-          <div className="equal-space-inputs">
-            <label>
-              First Name:
-                <input type="text" name="firstName" value='' onChange={props.handleInputChange} required />
-            </label>
+class CreateUserForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.submitter = this.submitter.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-            <label>
-              Last Name:
-                <input type="text" name="lastName" value='' onChange={props.handleInputChange} required />
-            </label>
+  submitter(event) {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: '/users/:username',
+      data: {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        username: this.state.username,
+        password: this.state.password, //TO DO: Encrypt the password; Bcrypt?
+      },
+      success: (data) => {
+        alert(`User created: ${username}`)
+        console.log('successfully created!')
+        //TO DO: route to the next page? logged in
+      },
+      error: (err) => {
+        console.log('err', err);
+      }
+    })
+      .done(this.props.handleUsernameSubmission)
+  }
 
-            <label>
-              Username:
-                <input type="text" name="username" value='' onChange={props.handleInputChange} required />
-            </label>
+  handleInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    console.log('typing', event.target.value)
+    this.setState({
+      [name]: [value]
+    })
+  }
 
-            <label>
-              Password:
-                <input type="text" name="password" value='' onChange={props.handleInputChange} required />
-            </label>
-          </div>
+  render() {
+    return (
+      <div className="wrapper" >
 
-          <div className="equal-space-buttons">
-            <Link to="/">
-              <input type="submit" className="btn btn-danger" value="Back"></input>
-            </Link>
-            <input type="submit" className="btn btn-danger" value="Create User" />
-          </div>
+        <h1 className="header">
+          Beat5238
+        </h1>
 
-        </form>
+        <div>
+          <form className="forms" onSubmit={e => this.submitter(e)}>
+            <div className="equal-space-inputs">
+              <label>
+                First Name:
+                  <input type="text" name="firstName" defaultValue='' onChange={this.handleInputChange} required />
+              </label>
+
+              <label>
+                Last Name:
+                  <input type="text" name="lastName" defaultValue='' onChange={this.handleInputChange} required />
+              </label>
+
+              <label>
+                Username:
+                  <input type="text" name="username" defaultValue='' onChange={this.handleInputChange} required />
+              </label>
+
+              <label>
+                Password:
+                  <input type="password" name="password" defaultValue='' onChange={this.handleInputChange} required />
+              </label>
+            </div>
+
+            <div className="equal-space-buttons">
+              <Link to="/">
+                <input type="button" className="btn btn-danger" value="Back"></input>
+              </Link>
+              <input type="submit" className="btn btn-danger" value="Create User" />
+            </div>
+
+          </form>
+        </div>
+
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default CreateUserForm;
--- react-client/src/components/LoginForm.jsx
+++ react-client/src/components/LoginForm.jsx
@@ -1,38 +1,78 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import $ from 'jquery';
 
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.submitter = this.submitter.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-function LoginForm(props) {
-  return (
-    <div className="wrapper">
-      <h1 className="header">
-        Beat5238
-      </h1>
-      <form className="forms" onSubmit={props.handleSubmit}>
-        <div className="equal-space-inputs">
-          <label>
-            Username:
-              <input type="text" name="username" value='' onChange={props.handleInputChange} required />
-          </label>
+  submitter(event) {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: '/users',
+      data: {
+        username: this.state.username,
+        password: this.state.password, //TO DO: Encrypt the password; Bcrypt?
+      },
+      success: (data) => {
+        alert(`User created: ${username}`)
+        console.log('successfully created!')
+        //TO DO: route to the next page? logged in
+      },
+      error: (err) => {
+        console.log('err', err);
+      }
+    })
+      .done(this.props.handleUsernameSubmission)
+  }
 
-          <label>
-            Password:
-              <input type="text" name="password" value='' onChange={props.handleInputChange} required />
-          </label>
-        </div>
+  handleInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    console.log('typing', event.target.value)
+    this.setState({
+      [name]: [value]
+    })
+  }
 
-        <div className="equal-space-buttons">
-          <input type="submit" className="btn btn-danger" value="Login" />
-          <Link to="/createuser">
-            <input type="submit" className="btn btn-danger" value="Sign Up" />
-          </Link>
-        </div>
+  render() {
+    return (
+      <div className="wrapper" >
+        <h1 className="header">
+          Beat5238
+        </h1>
+        <form className="forms" onSubmit={this.handleSubmit}>
+          <div className="equal-space-inputs">
+            <label>
+              Username:
+                <input type="text" name="username" defaultValue='' onChange={this.handleInputChange} required />
+            </label>
 
-        <div>
-        </div>
-      </form>
-    </div>
-  )
+            <label>
+              Password:
+                <input type="password" name="password" defaultValue='' onChange={this.handleInputChange} required />
+            </label>
+          </div>
+
+          <div className="equal-space-buttons">
+            <input type="submit" className="btn btn-danger" value="Login" />
+            <Link to="/createuser">
+              <input type="submit" className="btn btn-danger" value="Sign Up" />
+            </Link>
+          </div>
+
+        </form>
+      </div>
+    )
+  }
+
 }
 
 export default LoginForm;
