56e596bbdad28b7a01ffd89e539d92f487438368
jnthnch <jchu127@gmail.com>
2019-08-31 10:43:29 -0700

    Move games dasshboard to separate component

--- react-client/src/components/Dashboard.jsx
+++ react-client/src/components/Dashboard.jsx
@@ -1,40 +1,20 @@
 import React from 'react';
 import '../css/style.css'
-import gameSpreads from '../../gameSpreads';
-import SingleGame from './SingleGame.jsx';
+import GamesDashboard from './GamesDashboard.jsx';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      gameSpreads: gameSpreads
-    }
   };
 
   render() {
-    let games = this.state.gameSpreads.map((game, idx) =>
-      <SingleGame game={game} key={`key-${idx}`}></SingleGame>
-    )
     return (
       <div className="dashboard">
         <div className="dashboard-title">
           <p>Dashboard</p>
         </div>
 
-        <div className="games">
-          <p className="games-header">NFL Games ({this.state.gameSpreads.length}) - Week 1</p>
-          <div className="game-lines">
-            <div className="blank-space"></div>
-            <div className="game-lines-text">
-              <text className="spread">Spread</text>
-              <text className="money-line">Moneyline</text>
-              <text className="total">Total</text>
-            </div>
-          </div>
-          <div className="game-list">
-            {games}
-          </div>
-        </div>
+        <GamesDashboard></GamesDashboard>
 
         <div className="standings">
           <p>Standings</p>
---
+++ react-client/src/components/GamesDashboard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../css/style.css'
+import gameSpreads from '../../gameSpreads';
+import SingleGame from './SingleGame.jsx';
+
+class GamesDashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      gameSpreads: gameSpreads
+    }
+  };
+
+  render() {
+    let games = this.state.gameSpreads.map((game, idx) =>
+      <SingleGame game={game} key={`key-${idx}`}></SingleGame>
+    )
+    return (
+      <div className="games">
+        <p className="games-header">NFL Games ({this.state.gameSpreads.length}) - Week 1</p>
+        <div className="game-lines">
+          <div className="blank-space"></div>
+          <div className="game-lines-text">
+            <text className="spread">Spread</text>
+            <text className="money-line">Moneyline</text>
+            <text className="total">Total</text>
+          </div>
+        </div>
+        <div className="game-list">
+          {games}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GamesDashboard;
